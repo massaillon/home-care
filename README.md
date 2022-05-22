16b26e1d72037944dedbfbce1a40dd86bedd4c90
Massaillon Silue <96022653+massaillon@users.noreply.github.com>
2022-05-22 11:02:55 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,122 @@
+### Project Name
+
+  # Portfolio: validate contact form milestone 10 final 
+
+## Built With
+
+    - HTML
+    - CSS
+    - JAVASCRIPT
+
+
+## Getting Started  
+
+  This project aims at saving the form data in the local storage of the browser. That way when the user reloads the page the data they filled out in the form will be preserved.
+
+  Please follow the steps below for getting started and editing the files from your local machine.
+
+   Prerequisites
+
+     - You need to have the visual Code or some other code editors installed on your local machine
+
+     - You need to have a compatible browser with latest update running on your local machine
+
+      
+## Setup
+     
+   Set-up linters in your local env
+
+     * Setup for HTML-Checker called webhint
+
+        1 - run  "npm install --save-dev hint@6.x"
+        2 - Copy ".hintrc to the root directory of your project"
+        3 - Do not make any changes in config files
+        4 - Run "npx hint ." to run the HTML linter for a check
+        5 - Fix validation errors
+
+
+    
+    * Setup for CSS-checker called stylelint
+
+        1 - run  "npm install --save-dev stylelint@13.x stylelint-scss@3.x stylelint-config-standard@21.x stylelint-csstree-validator@1.x"
+
+        2 - Copy ".stylelintrc.json to the root directory of your project."
+        3 - Do not make any changes in config files
+        4 - Run "npx stylelint "**/*.{css,scss}"  to run the CSS linter for a check
+        5 - Fix validation errors.
+    
+    * Setup for JavaScript-checker called ESlint
+
+        1 - run  "npm install --save-dev eslint@7.x eslint-config-airbnb-base@14.x eslint-plugin-import@2.x babel-eslint@10.x"
+
+        2 - Copy " .eslintrc.json to the root directory of your project."
+        3 - Do not make any changes in config files
+        4 - Run "npx eslint ."  to run the javaScript linter for a check
+        5 - Fix validation errors.
+
+    
+## Usage
+
+    In the final milestone of our portfolio website, we will:
+
+    - We will implement the following interactions:
+        
+        When the user changes the content of any input field, the data is saved to the local storage.
+
+        When the user loads the page, if there is any data in the local storage the input fields are pre-filled with this data.
+
+    - We will use the following data model:
+        You must create a single JavaScript object with all the data from the entire form and save it in local storage. Do not create one local storage entry per input field.
+
+
+
+👤 Author
+
+    GitHub: @massaillon
+    LinkedIn: Massaillon Silue
+    Twitter: Massaillon Silue
+
+    **collaborator
+
+    ## Author
+👤 **Afolabi Akorede**
+
+- GitHub: [@brainconnect93](https://github.com/brainconnect93)
+- Twitter: [@brainconnect0](https://twitter.com/brainconnect0)
+- LinkedIn: [Afolabi Akorede](https://linkedin.com/in/brainconnect93)
+
+
+      
+ ##collaborator
+ 
+👤 **Afolabi Akorede**
+
+- GitHub: [@brainconnect93](https://github.com/brainconnect93)
+- Twitter: [@brainconnect0](https://twitter.com/brainconnect0)
+- LinkedIn: [Afolabi Akorede](https://linkedin.com/in/brainconnect93)
+
+Link to the Live website
+
+     https://massaillon.github.io/setup-and-mobile-version-skeleton/
+
+🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the issues page.
+Show your support
+
+Give a ⭐️ if you like this project!
+
+Acknowledgments
+
+    Hat tip to anyone whose code was used
+    Inspiration
+    etc
+
+📝 License
+
+This project is MIT licensed.
+
+
+    
---
+++ script.js
@@ -0,0 +1,143 @@
+/* Constants declarations for the Mobile menu */
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+/* Constants declarations for the Modal popUp */
+const modalWindow = document.querySelector('.modal');
+const modalWindow2 = document.querySelector('.modal2');
+const modalWindow3 = document.querySelector('.modal3');
+const modalWindow4 = document.querySelector('.modal4');
+const modalButton1 = document.querySelector('.seeProjBtn1');
+const modalButton2 = document.querySelector('.seeProjBtn2');
+const modalButton3 = document.querySelector('.seeProjBtn3');
+const modalButton4 = document.querySelector('.seeProjBtn4');
+const closeModal = document.querySelector('.close');
+const closeModal2 = document.querySelector('.close2');
+const closeModal3 = document.querySelector('.close3');
+const closeModal4 = document.querySelector('.close4');
+
+function mobileMenu() {
+  /* Open the menu and and change the Hamburger to a X on click */
+  if (navMenu.classList.contains('hide')) {
+    navMenu.classList.remove('hide');
+    hamburger.classList.toggle('active');
+  } else {
+    navMenu.classList.add('hide');
+    hamburger.classList.toggle('active');
+  }
+}
+hamburger.addEventListener('click', mobileMenu);
+/* For closing navigation on the click of each link */
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.add('hide');
+}));
+
+/* the function below opens and closes the modal popup window on click */
+function modalPopup() {
+  if (modalWindow.classList.contains('hide')) {
+    modalWindow.classList.remove('hide');
+  } else {
+    modalWindow.classList.add('hide');
+  }
+}
+
+function modalPopup2() {
+  if (modalWindow2.classList.contains('hide')) {
+    modalWindow2.classList.remove('hide');
+  } else {
+    modalWindow2.classList.add('hide');
+  }
+}
+
+function modalPopup3() {
+  if (modalWindow3.classList.contains('hide')) {
+    modalWindow3.classList.remove('hide');
+  } else {
+    modalWindow3.classList.add('hide');
+  }
+}
+
+function modalPopup4() {
+  if (modalWindow4.classList.contains('hide')) {
+    modalWindow4.classList.remove('hide');
+  } else {
+    modalWindow4.classList.add('hide');
+  }
+}
+/* Button settings for the eventLitener */
+modalButton1.addEventListener('click', modalPopup);
+modalButton2.addEventListener('click', modalPopup2);
+modalButton3.addEventListener('click', modalPopup3);
+modalButton4.addEventListener('click', modalPopup4);
+closeModal.addEventListener('click', modalPopup);
+closeModal2.addEventListener('click', modalPopup2);
+closeModal3.addEventListener('click', modalPopup3);
+closeModal4.addEventListener('click', modalPopup4);
+
+/* the function below closes the modal popup window on click anywhere in the modal */
+modalWindow.addEventListener('click', (a) => {
+  if (a.target.matches('.modal')) {
+    modalPopup();
+  }
+});
+
+modalWindow2.addEventListener('click', (b) => {
+  if (b.target.matches('.modal2')) {
+    modalPopup2();
+  }
+});
+
+modalWindow3.addEventListener('click', (c) => {
+  if (c.target.matches('.modal3')) {
+    modalPopup3();
+  }
+});
+
+modalWindow4.addEventListener('click', (d) => {
+  if (d.target.matches('.modal4')) {
+    modalPopup4();
+  }
+});
+
+/* Form Validation Code */
+
+const form = document.getElementById('form-Cont');
+const email = document.getElementById('email');
+const errorMessage = document.querySelector('.error-message');
+/* the below function will check if the email entered is fully lowerCase */
+function emailCheckLowerCase(e) {
+  if (email.value !== email.value.toLowerCase()) {
+    errorMessage.classList.remove('hide');
+    e.preventDefault();
+  } else {
+    errorMessage.classList.add('hide');
+  }
+}
+form.addEventListener('change', emailCheckLowerCase);
+form.addEventListener('submit', emailCheckLowerCase);
+
+/* Local Storage using Objects */
+
+const inputName = document.getElementById('name');
+const inputEmail = document.getElementById('email');
+const inputMessage = document.getElementById('message');
+// Create an onsubmit event listener for the form
+form.onsubmit = () => {
+  // Capture the data entered by the user in the form
+  // Store it inside an object
+  const formElement = {
+    username: inputName.value,
+    email: inputEmail.value,
+    userTextInput: inputMessage.value,
+  };
+  // Store the object to the local storage
+  localStorage.setItem('storelocal', JSON.stringify(formElement));
+};
+// Get the object to the local storage
+const localData = JSON.parse(localStorage.getItem('storelocal'));
+
+// put data into the form
+
+inputName.value = localData.username;
+inputEmail.value = localData.email;
+inputMessage.value = localData.userTextInput;
